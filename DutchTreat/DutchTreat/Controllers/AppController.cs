4b0c01637e0ba2474ebaa84bdcbf9d62b6b5c934
Victor Sun <vsun@ostusa.com>
2018-10-22 16:18:26 -0400

    V

--- DutchTreat/DutchTreat/Controllers/AppController.cs
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -3,52 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using DutchTreat.Services;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DutchTreat.Controllers
 {
-  public class AppController : Controller
-  {
-    private readonly IMailService _mailService;
+	public class AppController : Controller
+	{
+		private readonly IMailService _mailService;
+		private readonly DutchContext _context;
 
-    public AppController(IMailService mailService)
-    {
-      _mailService = mailService;
-    }
+		public AppController(IMailService mailService, DutchContext context)
+		{
+			_mailService = mailService;
+			_context = context;
+		}
 
-    public IActionResult Index()
-    {
-      return View();
-    }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-    [HttpGet("contact")]
-    public IActionResult Contact()
-    {
-      return View();
-    }
+		[HttpGet("contact")]
+		public IActionResult Contact()
+		{
+			return View();
+		}
 
-    [HttpPost("contact")]
-    public IActionResult Contact(ContactViewModel model)
-    {
-      if (ModelState.IsValid)
-      {
-        // Send the email
-        _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
-        ViewBag.UserMessage = "Mail Sent";
-        ModelState.Clear();
-      }
+		[HttpPost("contact")]
+		public IActionResult Contact(ContactViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				// Send the email
+				_mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+				ViewBag.UserMessage = "Mail Sent";
+				ModelState.Clear();
+			}
 
-      return View();
-    }
+			return View();
+		}
 
-    public IActionResult About()
-    {
-      ViewBag.Title = "About Us";
+		public IActionResult About()
+		{
+			ViewBag.Title = "About Us";
 
-      return View();
-    }
+			return View();
+		}
 
-  }
+		public IActionResult Shop()
+		{
+			//var results = _context.Products
+			//	.OrderBy(p => p.Category)
+			//	.ToList();
+			//return View();
+
+			var results = from p in _context.Products
+						  orderby p.Category
+						  select p;
+
+			return View(results.ToList());
+		}
+	}
 }
